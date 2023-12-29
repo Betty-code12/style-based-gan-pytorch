48125f191ed91e264b1cdf6655afebb9ee987c30
Betty-code12 <68535813+Betty-code12@users.noreply.github.com>
2023-12-29 23:10:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def train(args, dataset, generator, discriminator):
     adjust_lr(g_optimizer, args.lr.get(resolution, 0.001))
     adjust_lr(d_optimizer, args.lr.get(resolution, 0.001))
 
-    pbar = tqdm(range(3_000_000))
+    pbar = tqdm(range(30_000))
 
     requires_grad(generator, False)
     requires_grad(discriminator, True)
@@ -261,7 +261,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--phase',
         type=int,
-        default=300_000,
+        default=30_000,
         help='number of samples used for each training phases',
     )
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
